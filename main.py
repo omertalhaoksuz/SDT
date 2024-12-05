7dbc571dabe9cc6307787d742755f5375b45c1ef
omer talha oksuz <omertalha_oksuz21@erdogan.edu.tr>
2024-12-05 14:35:07 +0300

    Added table sorting

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import random
 
 def create_table():
     table = [random.randint(1, 100) for _ in range(5)]
-    for item in table:
-        print(item)
+    print("Unsorted table:", table)
+    table.sort()
+    print("Sorted table:", table)
 
 if __name__ == "__main__":
     create_table()
-
